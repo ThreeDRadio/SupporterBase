06aa3f4ccaf92708dbbb4e35f301be69ad4279ff
Michael Marner <michael.marner@unisa.edu.au>
2013-08-23 15:26:57 +0930

    Test database configuration.

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'threedradio';
+$db['default']['password'] = 'threedradio';
+$db['default']['database'] = 'supporters';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
