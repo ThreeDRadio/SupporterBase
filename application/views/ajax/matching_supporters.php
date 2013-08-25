f3a4dedfc6c3646e04e3d496d63f1dc15d427aec
Michael Marner <michael.marner@unisa.edu.au>
2013-08-25 22:39:10 +0930

    Add all the fields to the AJAX output.

--- application/views/ajax/matching_supporters.php
+++ application/views/ajax/matching_supporters.php
@@ -4,6 +4,13 @@
         <supporter_id><?php echo $supporter['supporter_id'] ?></supporter_id>
         <first_name><?php echo $supporter['first_name'] ?></first_name>
         <last_name><?php echo $supporter['last_name'] ?></last_name>
+        <address1><?php echo $supporter['address1'] ?></address1>
+        <address2><?php echo $supporter['address2'] ?></address2>
+        <town><?php echo $supporter['town'] ?></town>
+        <postcode><?php echo $supporter['postcode'] ?></postcode>
+        <state><?php echo $supporter['state'] ?></state>
+        <phone><?php echo $supporter['phone'] ?></phone>
+        <email><?php echo $supporter['email'] ?></email>
     </supporter>
 
 <?php endforeach; ?>
