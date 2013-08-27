6d80358c97f101a2dbbb831e4414a031ef28bc18
Michael Marner <michael.marner@unisa.edu.au>
2013-08-27 10:28:00 +0930

    Latest database schema.

--- supporters.sql
+++ supporters.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 25, 2013 at 03:04 PM
+-- Generation Time: Aug 27, 2013 at 02:57 AM
 -- Server version: 5.5.31
 -- PHP Version: 5.4.16
 
@@ -38,13 +38,6 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
   KEY `last_activity_idx` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `ci_sessions`
---
-
-INSERT INTO `ci_sessions` (`session_id`, `ip_address`, `user_agent`, `last_activity`, `user_data`) VALUES
-('b0991be2c81471402698b8f524ede263', '::1', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:23.0) Gecko/20100101 Firefox/23.0', 1377435715, 'a:2:{s:7:"user_id";s:1:"9";s:9:"logged_in";b:1;}');
-
 -- --------------------------------------------------------
 
 --
@@ -61,18 +54,16 @@ CREATE TABLE IF NOT EXISTS `supporters` (
   `state` text NOT NULL,
   `postcode` varchar(12) NOT NULL,
   `country` text NOT NULL,
-  `phone` text NOT NULL,
+  `phone_mobile` text NOT NULL,
   `email` text NOT NULL,
+  `phone_home` text NOT NULL,
+  `phone_work` text NOT NULL,
+  `gender` char(1) NOT NULL,
+  `dob` text NOT NULL,
+  `notes` text NOT NULL,
   PRIMARY KEY (`supporter_id`),
   KEY `last_name` (`last_name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `supporters`
---
-
-INSERT INTO `supporters` (`supporter_id`, `first_name`, `last_name`, `address1`, `address2`, `town`, `state`, `postcode`, `country`, `phone`, `email`) VALUES
-(1, 'Michael', 'Marner', '2 Oxford Crescent', '', 'Modbury', 'SA', '5092', 'Australia', '0438984432', 'michael@20papercups.net');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=125 ;
 
 -- --------------------------------------------------------
 
@@ -89,7 +80,7 @@ CREATE TABLE IF NOT EXISTS `transactions` (
   `type` enum('sub','sub_concession','member','member_concession') NOT NULL,
   PRIMARY KEY (`transaction_id`),
   KEY `user_id` (`user_id`,`supporter_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 -- --------------------------------------------------------
 
@@ -105,13 +96,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `username`, `password`, `salt`) VALUES
-(9, 'mmarner', 'fabc0238113b58bf64e40d2dca707cbb184902a9d9da100d204417f33df6f5e0', 'i9eJ/6lzTh6xt86WoF5TktvBqe8jVicF0sLv3SlKM/OAagXmjrKSagnPfy4oXMODPSDEnC5tAxc8B0hQQAg8HA==');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
