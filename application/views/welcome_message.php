0b8e57639017357a10a4484e1a196f051013239c
Michael Marner <michael.marner@unisa.edu.au>
2013-09-25 19:30:08 +0930

    Link to export current members.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,7 +7,7 @@
 
 <h2>Stats</h2>
 
-<p><b>Current Members:</b> <?php echo $activeMembers; ?><br />
+<p><b>Current Members:</b> <?php echo $activeMembers; ?> <a href="<?php echo site_url('export/current_members') ?>">Export</a><br />
 <p><b>Unrenewed Members:</b> <?php echo $expiredMembers; ?></p>
 
 <p><b>Current Subscribers:</b> <?php echo $activeSubs; ?><br />
