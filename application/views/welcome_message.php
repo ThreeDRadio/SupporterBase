763fcab532bd0100a55e80ad238ef0f285949088
Michael Marner <michael@20papercups.net>
2015-02-18 09:55:02 +1030

    Added browse link for current members.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -5,7 +5,7 @@
 
 <h2>Stats</h2>
 
-<p><b>Current Members:</b> <?php echo $activeMembers; ?> <a href="<?php echo site_url('export/current_members') ?>">Export</a><br />
+<p><b>Current Members:</b> <?php echo $activeMembers; ?> <a href="<?php echo site_url('supporters/browse/members/current') ?>">Browse</a> | <a href="<?php echo site_url('export/current_members') ?>">Export</a><br />
 <p><b>Unrenewed Members:</b> <?php echo $expiredMembers; ?>:<a href="<?php echo site_url('supporters/browse/members/expired') ?>">Browse</a> | <a href="<?php echo site_url('export/expired_members') ?>">Export</a></p>
 
 <p><b>Current Subscribers:</b> <?php echo $activeSubs; ?> <a href="<?php echo site_url('supporters/browse/subscribers/current') ?>">Browse</a> | <a href="<?php echo site_url('export/current_subscribers') ?>">Export</a><br />
